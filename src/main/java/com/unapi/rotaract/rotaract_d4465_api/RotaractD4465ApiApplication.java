4062d3de5ca06953249b298238836d0f520b66a9
Joan Piero Solignac Lovera <155686793+JoanSolignac@users.noreply.github.com>
2025-10-12 22:36:59 -0500

    chore: cargar variables de entornor en raíz del proyecto

--- src/main/java/com/unapi/rotaract/rotaract_d4465_api/RotaractD4465ApiApplication.java
+++ src/main/java/com/unapi/rotaract/rotaract_d4465_api/RotaractD4465ApiApplication.java
@@ -3,10 +3,22 @@ package com.unapi.rotaract.rotaract_d4465_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class RotaractD4465ApiApplication {
 
 	public static void main(String[] args) {
+
+		// Cargar variables del archivo .env
+        Dotenv dotenv = Dotenv.load();
+
+        // Asignar propiedades de entorno a spring
+        System.setProperty("DB_URL", dotenv.get("MYSQL_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("MYSQL_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
+        System.setProperty("DB_DRIVER", "com.mysql.cj.jdbc.Driver");
+
 		SpringApplication.run(RotaractD4465ApiApplication.class, args);
 	}
 
