7394ac7f228a3deb181df27c9fc4d86c63674f04
Joan Piero Solignac Lovera <155686793+JoanSolignac@users.noreply.github.com>
2025-10-12 22:28:05 -0500

    First Commit

---
+++ src/main/java/com/unapi/rotaract/rotaract_d4465_api/RotaractD4465ApiApplication.java
@@ -0,0 +1,13 @@
+package com.unapi.rotaract.rotaract_d4465_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaractD4465ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotaractD4465ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unapi/rotaract/rotaract_d4465_api/RotaractD4465ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unapi.rotaract.rotaract_d4465_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotaractD4465ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
