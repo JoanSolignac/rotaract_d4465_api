82e39cf859e8da00446f25476e67fcc1adf549d6
Joan Piero Solignac Lovera <155686793+JoanSolignac@users.noreply.github.com>
2025-10-13 16:44:40 -0500

    feat: agregar el controlador de autenticación

---
+++ src/main/java/com/unapi/rotaract/rotaract_d4465_api/auth/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.unapi.rotaract.rotaract_d4465_api.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.unapi.rotaract.rotaract_d4465_api.auth.dtos.AuthResponseDto;
+import com.unapi.rotaract.rotaract_d4465_api.auth.dtos.LoginRequestDto;
+import com.unapi.rotaract.rotaract_d4465_api.auth.dtos.RegistroRequestDto;
+import com.unapi.rotaract.rotaract_d4465_api.auth.service.AuthService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    // Métodos para manejar las solicitudes de autenticación
+
+    public ResponseEntity<AuthResponseDto> login(@Valid @RequestBody LoginRequestDto loginRequestDto)
+    {
+        AuthResponseDto response = authService.login(loginRequestDto);
+        return ResponseEntity.ok(response);
+    }
+
+    public ResponseEntity<AuthResponseDto> register(@Valid @RequestBody RegistroRequestDto registroRequestDto)
+    {
+        AuthResponseDto response = authService.register(registroRequestDto);
+        return ResponseEntity.ok(response);
+    }
+
+}
