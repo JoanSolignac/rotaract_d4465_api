50997412c997531a2b2146a8c80cfcc8fcf5ec09
Eisritome10 <2225265@unapiquitos.edu.pe>
2025-10-22 17:45:49 -0500

    chore: prueba de subido de ConvocatoriaEntity con la creacion de las carpetas y un poco de ConvocatoriaController de prueba

--- src/main/java/com/unapi/rotaract/rotaract_d4465_api/convocatoria/controller/ConvocatoriaController.java
+++ src/main/java/com/unapi/rotaract/rotaract_d4465_api/convocatoria/controller/ConvocatoriaController.java
@@ -1,4 +1,15 @@
 package com.unapi.rotaract.rotaract_d4465_api.convocatoria.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/convocatorias")
+@RequiredArgsConstructor
+@Tag(name = "Convocatorias", description = "Operaciones para consultar y gestionar convocatorias")
 public class ConvocatoriaController {
+    private final Con
+
 }
--- src/main/java/com/unapi/rotaract/rotaract_d4465_api/convocatoria/entity/ConvocatoriaEntity.java
+++ src/main/java/com/unapi/rotaract/rotaract_d4465_api/convocatoria/entity/ConvocatoriaEntity.java
@@ -1,10 +1,13 @@
 package com.unapi.rotaract.rotaract_d4465_api.convocatoria.entity;
 
+import com.unapi.rotaract.rotaract_d4465_api.club.entity.ClubEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "convocatorias")
@@ -13,8 +16,37 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 
-public class ClubEntity {
+public class ConvocatoriaEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private long id;
+
+    @Column(nullable = false, length = 155)
+    private String titulo;
+
+    @Column(nullable = false, length = 500)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private LocalDate fechaInicio;
+
+    @Column(nullable = false)
+    private LocalDate fechaFin;
+
+    @Column(nullable = false, length = 155)
+    private String lugar;
+
+    @Column(nullable = false)
+    private Integer capacidad;
+
+    @Column(nullable = false)
+    private LocalDate inicioInscripcion;
+
+    @Column(nullable = false)
+    private LocalDate finInscripcion;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "club_id", nullable = false)
+    private ClubEntity club;
+
 }
